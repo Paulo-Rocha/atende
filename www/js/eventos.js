7291ced9cf63859d19324e3554eac56b7551cb26
Paulo-Rocha <pajor65@gmail.com>
2018-07-12 20:01:18 -0300

    s

--- www/js/eventos.js
+++ www/js/eventos.js
@@ -16,15 +16,15 @@ function selecaoTribo(tribo){
     switch(tribo) {
         case 1:
             div.style.backgroundColor =  "#880019";
-            
-        break;
+           
         case 2:
             div.style.backgroundColor = "#fff200";
-            
+            document.getElementById('logoTribus').innerHTML = "<img src='../images/supermercadobh.png' height='24' data-inline='true' style='float: left;'>";
+             
         break;
         default:
             div.style.backgroundColor = "#f3f3f3";
-            
+           
     }
     
 }       
