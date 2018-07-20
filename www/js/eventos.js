87eb63dab91de7271717d21e0a2599954add0539
Paulo-Rocha <pajor65@gmail.com>
2018-07-20 18:39:17 -0300

    test

--- www/js/eventos.js
+++ www/js/eventos.js
@@ -1,31 +1,10 @@
 
 $(document).ready(function() {
-
   $("#select-tribo").bind( "change", function(event, ui) {
     var x = $(this).find(":selected").val();
     selecaoTribo(x);
-
   });
-
-
 });
-function selecaoTribo(tribo){
-    tribo = parseInt(tribo);
-    var div = document.getElementById( 'coresTribo' );
 
-    switch(tribo) {
-        case 1:
-            div.style.backgroundColor =  "#880019";
-           
-        case 2:
-            div.style.backgroundColor = "#fff200";
-            document.getElementById('logoTribus').innerHTML = "<img src='../images/supermercadobh.png' height='24' data-inline='true' style='float: left;'>";
-             
-        break;
-        default:
-            div.style.backgroundColor = "#f3f3f3";
-           
-    }
-    
-}       
+
 
--- www/js/index.js
+++ www/js/index.js
@@ -14,7 +14,12 @@
         //app.receivedEvent('deviceready');
         
         var conn = checkConnection();
-        conecta = conn.split("|");   conecta[0]=2;
+        conecta = conn.split("|"); 
+
+//remover a linha abaixo
+          conecta[0]=2;
+
+
         document.getElementById('status_conn').innerHTML = "<img src='img/"+conecta[0]+"conn.png' width='32' height='32' data-inline='true'>"+conecta[1];
         conecta[0] = parseInt(conecta[0]);
         conecta[0]!==0 ? dataCloud(conecta[1]) : app.ok_conn_sync();
@@ -24,12 +29,23 @@
     ok_conn_sync: function() { 
         //buscar dados local do usuário
         var x = localStorage.getItem("user_id");
-
+        
         cadastro = x == null ? false : true;
 
         if(cadastro){ 
             dataConn = JSON.parse( localStorage.getItem('dataConn') );
             document.getElementById('cadastro_ok').innerHTML = "<img src='img/user.png' width='32' height='32' data-inline='true'>"+dataConn.nome;
+            var tribos=dataConn.tribos;
+            
+            //Construindo o menu de tribos
+            var item = "";    
+            $.each(tribos, function(index, value) {
+                item += "<option value='"+index+"'>"+value.identifica+"</option> ";
+            });
+            //alert(item);
+            var sel_Tribo = $('#select-tribo');
+                sel_Tribo.empty().append(item);
+                sel_Tribo.selectmenu().selectmenu('refresh');
         }
 
 
@@ -39,19 +55,7 @@
 
             if(cadastro){//Já tem cadastro - exibir informações na tela
                 document.getElementById("cadastrado").style.display = "block";
-                document.getElementById('user_name').innerHTML = dataConn.nome;
-                //document.getElementById('tribo').innerHTML = dataConn.tribo;
-                document.getElementById('bonus').innerHTML = dataConn.bonus;
-                document.getElementById('valor_bonus').innerHTML = parseInt(dataConn.bonus)*dataConn.cota_bonus;
-                document.getElementById('bloqueados').innerHTML = dataConn.bloqueados;
-                document.getElementById('valor_bloqueado').innerHTML = parseInt(dataConn.bloqueados)*dataConn.cota_bonus;
-                
-                document.getElementById('ultima_consulta').innerHTML = dataConn.ultima_consulta;
-
-
-
-
-
+                selecaoTribo(dataConn.default_tribo);
 
                 //document.getElementById('conn').innerHTML = conecta[1];
             }else{//Não tem cadastro
@@ -62,15 +66,7 @@
         if(conecta[0] === 2) { //Conexão via celular ... consome franquia
             if (cadastro) {
                 document.getElementById("cadastrado").style.display = "block";
-                document.getElementById('user_name').innerHTML = dataConn.nome;
-                //document.getElementById('tribo').innerHTML = dataConn.tribo;
-                document.getElementById('bonus').innerHTML = dataConn.bonus;
-                document.getElementById('valor_bonus').innerHTML = Number(parseInt(dataConn.bonus)*dataConn.cota_bonus).toFixed(2);
-                document.getElementById('bloqueados').innerHTML = dataConn.bloqueados;
-                document.getElementById('valor_bloqueado').innerHTML = Number(parseInt(dataConn.bloqueados)*dataConn.cota_bonus).toFixed(2);
-                document.getElementById('ultima_consulta').innerHTML = dataConn.ultima_consulta;
-
-
+                selecaoTribo(dataConn.default_tribo);
             }else{
                 //abrirTribus('http://www.atendeweb.net/atende/admin/svcs/_page_index.php?id=7&dom=tribus');  
                 var url ='http://www.atendeweb.net/atende/admin/svcs/_page_index.php?id=7&dom=tribus'
@@ -208,15 +204,24 @@ function dataCloud(conexao){
            },
            timeout: 3000,    
                 success: function(retorno){
+                    //alert(retorno);
                     //document.getElementById('status').innerHTML = retorno;
                     var obj = JSON.parse(retorno);
                     //$.each(obj, function(index, value){
                     //});
+                    document.getElementById('status').innerHTML = JSON.stringify(obj.favorito);
 
-                    //alert (texto);
                     if(obj.login.length > 0){
                         localStorage.setItem("dataConn", JSON.stringify( obj ) );
                         localStorage.setItem("user_id",  obj.user_id);
+                        //document.getElementById('status').innerHTML = "User:"+obj.user_id+"|Tribo:"+obj.tribos[obj.default_tribo].identifica;
+ 
+                        if(Object.keys(obj.favorito).length>0){
+                            criarFavoritos(obj.favorito);
+                        }else{
+                            $("#listaFavorito").html("<p>Você ainda não adicionou favoritos!</p>");
+                        }
+
                     }else{
                         localStorage.removeItem("user_id");
                     }    
@@ -240,3 +245,59 @@ function teste(){
      str += "\nconecta[1]:"+conecta[1];
      alert(str);  
 }       
+function selecaoTribo(tribo){
+    tribo = parseInt(tribo);
+    var div = document.getElementById( 'coresTribo' );
+
+    div.style.backgroundColor =  "#"+dataConn.tribos[tribo].cor;
+
+
+    document.getElementById('user_name').innerHTML = dataConn.nome;
+    //document.getElementById('tribo').innerHTML = dataConn.tribo;
+    document.getElementById('bonus').innerHTML = dataConn.tribos[tribo].bonus;
+    document.getElementById('valor_bonus').innerHTML =     Number(parseInt(dataConn.tribos[tribo].bonus)     *dataConn.tribos[tribo].cota_bonus).toFixed(2);
+    document.getElementById('bloqueados').innerHTML = dataConn.tribos[tribo].bloqueados;
+    document.getElementById('valor_bloqueado').innerHTML = Number(parseInt(dataConn.tribos[tribo].bloqueados)*dataConn.tribos[tribo].cota_bonus).toFixed(2);
+    document.getElementById('ultima_consulta').innerHTML = dataConn.tribos[tribo].ultima_consulta;
+    //alert("images/logo"+tribo+".png");
+    $("#imgLogo").attr("src","images/logo"+tribo+".png");
+}       
+
+function criarFavoritos(fav){
+    var total = Object.keys(fav).length;
+    $('#listaFavorito').append($('<div>').attr({ 'data-role': 'collapsible-set',
+        'class':'ui-collapsible-set', 
+        'data-theme':'a',
+        'data-content-theme':'b',
+        'data-inset':'false',
+        'id': 'anuncios' }));
+    $.each(fav, function(index, value) {
+        //item += "<option value='"+index+"'>"+value.identifica+"</option> ";
+        //alert(value.titulo);
+        var texto = "<img class='star' src='img/"+value.vlr_aval+"star.png' height='20'>";
+            texto += "<span class='avals'>&nbsp;Avaliações:&nbsp;<span class='ui-li-count'>"+value.qua_aval+"</span>&nbsp;&nbsp;</span>";
+
+        var tel ="<ul data-role='listview' data-inset='true'>";
+            
+        $.each(value.operadora, function(ind, val) {  
+           tel += "<li data-icon='phone'><a href='tel:+"+ind+"'><img src='img/tel_"+val+".png' class='ui-li-thumb'>\
+                  <h2>"+val+"</h2><p>"+ind+"</p><p class='ui-li-aside'>"+value.identifica[ind]+"</p>\
+                  </a></li>\
+                  <li data-role='list-divider'></li>";
+        });
+           tel += "</ul>";
+
+
+
+        ($('<div>')
+            .attr({
+            'data-role': 'collapsible',
+            'data-collapsed': 'true'
+            })
+            .html("<h3>"+value.titulo+"</h3>"+texto+"<p class='atividade'>"+value.atividade+"</p><p class='msg_c'>"+value.msg_comercial+"</p><p class='msg_ben'>"+value.msg_beneficio+"</p><p>"+tel+"</p>")     
+            .appendTo('#anuncios'));
+
+    });
+    $('#listaFavorito').collapsibleset().trigger('create');   
+}
+
